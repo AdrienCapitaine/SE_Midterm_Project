a9267a2d7c39817811c6cb7dc99fd5af0506b8e6
damiendth <damien.duthou@gmail.com>
2024-05-01 11:25:46 +0900

    comments updated

--- airports.py
+++ airports.py
@@ -4,11 +4,32 @@ import requests
 from math import radians, cos, sin, asin, sqrt
 
 def haversine(lon1, lat1, lon2, lat2):
-    """
-    float lon1, float lat1, float lon2, float lat2 -> float distance
+     """
+    Calculates the distance in kilometers between two points on the Earth using the Haversine formula.
 
-    Calculates the distance in kilometers between two points 
-    on the earth.
+    Parameters:
+    - lon1 (float): Longitude of the first point in degrees.
+    - lat1 (float): Latitude of the first point in degrees.
+    - lon2 (float): Longitude of the second point in degrees.
+    - lat2 (float): Latitude of the second point in degrees.
+
+    Returns:
+    - distance (float): The distance between the two points in kilometers.
+
+    Formula:
+    - The function applies the Haversine formula to calculate the distance between two points on a sphere.
+    - The formula takes into account the curvature of the Earth's surface.
+
+    Dependencies:
+    - math.radians: Converts angles from degrees to radians.
+    - math.sin: Computes the sine of an angle.
+    - math.cos: Computes the cosine of an angle.
+    - math.sqrt: Computes the square root of a number.
+    - math.asin: Computes the arcsine of a value.
+
+    Constants:
+    - r (float): Radius of the Earth in kilometers. The function assumes a spherical Earth with a radius of 6371 kilometers.
+
     """
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
     dlon = lon2 - lon1 
@@ -21,15 +42,40 @@ def haversine(lon1, lat1, lon2, lat2):
 
 def get_airports(destinationCity,coords):
     """
-    str destinationCity, tpl(float latitude,float longitude)  coords-> list[dict] airports
-    
-    This function returns a list dictionnaries describing airports that are affiliated to the destination city as well as within 50 km of the input coordinates.
+    Retrieves information about airports affiliated with the destination city and within 50 kilometers of the input coordinates.
+
+    Parameters:
+    - destinationCity (str): The name of the destination city.
+    - coords (tuple(float, float)): Latitude and longitude coordinates of the location.
+
+    Returns:
+    - airports (list[dict]): A list of dictionaries containing information about airports that meet the specified criteria.
+        Each dictionary includes various details about the airport, such as name, ICAO code, latitude, longitude, city, country, and distance from the input coordinates.
+
+    Dependencies:
+    - requests: Used to perform HTTP requests.
+    - urllib.parse: Used to encode parameters for the API request.
+    - haversine: A function to calculate the distance between two points on the Earth's surface.
+
+    API Key:
+    - The function requires an API key for authentication with the API-Ninjas API. 
+      The API key should be provided in the function code.
+
+    API Endpoint:
+    - The function utilizes the API-Ninjas API endpoint '/v1/airports' to fetch information about airports.
+
+    Radius:
+    - The function searches for airports within a 50-kilometer radius of the input coordinates.
+
+    Error Handling:
+    - If the API key is invalid or has expired, the function raises a ValueError.
+
     """
     key = "LeLHVfX2J+40J2TpuXen7A==JckrqmRp99HGwL2a"
 
     url = "https://api.api-ninjas.com"
     uri_nearest_airport = "/v1/airports"
-    radius = 50 # 0.5 mile radius around nodes 
+    radius = 50 
 
     result = []
     res = [1]
@@ -44,25 +90,22 @@ def get_airports(destinationCity,coords):
             break
         i+= len(res)
 
-    #print(len(airports))
     if len(airports) != 0:
         try:
             a = airports[0]["latitude"]
         except:
             raise ValueError("Api key is invalid or has expired. Please request a new one.")
-    #print("End of request")
+
     for airport in airports:
         distance = haversine(coords[0],coords[1], float(airport["latitude"]), float(airport["longitude"]))
 
         
         if distance <= radius:
-            #print(airport["name"], '/',airport["icao"],'/',distance, '/',airport["latitude"],'/', airport["longitude"] , '/',airport["city"] , '/', airport["country"], '/', distance)
+            
             airport["distance"] = distance
             result.append(airport)
 
-    #print(result)
     return result
 
 # Example
 #get_airports("London", (51.5073509,-0.1277583)) # gets airports in London that are within 50 km of the input coordinates
-#get_airports("Moscow", (55.7558, 37.6176)) # gets airports in Moscow that are within 50 km of the input coordinates
